70953193a958b22efddca75961570783ad9b6db4
Szymon Witold Latawiec <slatawie@student.wszib.edu.pl>
2022-10-02 15:14:59 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/app/mypackage/MyClass2.java
@@ -0,0 +1,16 @@
+package pl.edu.wszib.app.mypackage;
+
+import pl.edu.wszib.app.MyClass;
+
+public class MyClass2 extends MyClass {
+
+//    private pl.edu.wszib.app.MyClass myClass;
+
+    public void run() {
+
+//        myClass.run4();
+        field3 = "Field3 from extended";
+        run3();
+    }
+
+}
